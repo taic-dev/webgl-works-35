d98ba01272cee4124ec5705f5ebf7fac382f85b0
ohmoto <c.t.o.taishi.0530@gmail.com>
2025-11-27 11:11:27 +0900

    fix

--- src/assets/ts/webgl/BloomEffect.ts
+++ src/assets/ts/webgl/BloomEffect.ts
@@ -2,7 +2,7 @@ import * as THREE from "three";
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
 import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass.js";
 // @ts-ignore
-import { FXAAPass } from 'three/examples/jsm/postprocessing/FXAAPass';
+import { SMAAPass } from "three/examples/jsm/postprocessing/SMAAPass.js";
 import { OutputPass } from "three/examples/jsm/postprocessing/OutputPass.js";
 import { Setup } from "./Setup";
 import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
@@ -14,7 +14,7 @@ export class BloomEffect {
   setup: Setup;
   renderPassBloom: RenderPass | null;
   renderPassBase: RenderPass | null;
-  fxaaPass: FXAAPass
+  smaaPass: SMAAPass
   unrealBloomPass: UnrealBloomPass | null
   outPass: OutputPass;
   shaderPass: ShaderPass | null;
@@ -25,7 +25,7 @@ export class BloomEffect {
     this.setup = setup;
     this.renderPassBloom = null;
     this.renderPassBase = null;
-    this.fxaaPass = new FXAAPass();
+    this.smaaPass = new SMAAPass();
     this.unrealBloomPass = null;
     this.outPass = new OutputPass();
     this.shaderPass = null;
@@ -55,7 +55,7 @@ export class BloomEffect {
     
     this.effectComposer2.addPass(this.renderPassBase);
     this.effectComposer2.addPass(this.shaderPass);
-    this.effectComposer2.addPass(this.fxaaPass);
+    this.effectComposer2.addPass(this.smaaPass);
   }
 
   setMaterial() {
